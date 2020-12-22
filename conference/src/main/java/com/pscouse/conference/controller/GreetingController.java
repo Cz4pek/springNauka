ea41a7ac5faa0974738d2c5dda2e23a17114e38c
Cz4pek <czarek360@gmail.com>
2020-12-22 23:50:59 +0100

    some changes

--- conference/src/main/java/com/pscouse/conference/controller/GreetingController.java
+++ conference/src/main/java/com/pscouse/conference/controller/GreetingController.java
@@ -1,2 +1,17 @@
-package com.pscouse.conference.controller;public class GreetingController {
+package com.pscouse.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("greeting")
+    public String greeting(Map<String, Object> model) {
+        model.put("message", "Grasz w ping pong?");
+        return "greeting";
+    }
+
 }
